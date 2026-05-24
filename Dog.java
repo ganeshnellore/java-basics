7a906d469867b53e32c58699fe976f78159c121e
= <=ganeshnellore59@gmail.com>
2026-05-24 14:38:49 +0530

    Dog file

---
+++ Dog.java
@@ -0,0 +1,19 @@
+package variables;
+
+public class Dog {
+	String breed = "german shepard";
+	int weight = 30;
+	int height = 7;
+
+	public static void main(String[] args) {
+//		int age = 15;
+//		Dog dog = new Dog();
+		System.out.println(new Dog().breed);
+//		System.out.println(dog.weight);
+//		System.out.println(dog.nickName);
+
+	}
+
+	String nickName = "puppy";
+	
+}
